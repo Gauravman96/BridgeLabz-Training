b74bba48160428880164cdf8fd375446d5504549
gauravman96 <gauravmangal9660@gmail.com>
2025-12-18 15:48:22 +0530

    [gaurav mangal] HeightConverte added

---
+++ core-java-practice/gcr-codebase/programming-elements/HeightConverte.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class HeightConverte {
+    public static void main(String[] args) {
+         Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter your height in centimeters: ");
+         double heightCm = input.nextDouble();
+
+        // Convert cm to inches
+         double totalInches = heightCm / 2.54;
+
+        // Convert inches to feet and remaining inches
+         double feet = (int) (totalInches / 12);
+         double inches = totalInches % 12;
+
+        System.out.println("Your Height in cm is " + heightCm +
+                           " while in feet is " + feet +
+                           " and inches is " + inches);
+    }
+}
