34d4bea0c028b21d9ece7329d8caf9a3f2bbd1b9
gauravman96 <gauravmangal9660@gmail.com>
2025-12-18 11:43:51 +0530

    [gaurav mangal]question 11 added

---
+++ core-java-practice/gcr-codebase/programming-elements/Question11.java
@@ -0,0 +1,9 @@
+public class Question11 {
+    public static void main(String[] args) {
+        int birthyear= 2000;
+        int currentyear= 2024;
+        int year = currentyear- birthyear;
+        System.out.println("Harry's age is "+ year);
+        
+    }
+}
