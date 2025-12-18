b3f1c1e07bf502e00a782a0649f088c84f14809a
gauravman96 <gauravmangal9660@gmail.com>
2025-12-18 12:20:56 +0530

    [gaurav mangal] question12

---
+++ core-java-practice/gcr-codebase/programming-elements/Question12.java
@@ -0,0 +1,10 @@
+
+public class Question12 {
+    public static void main(String[] args) {
+        int mathmarks=94;
+        int physicsmarks=95;
+        int chemistrymarks= 96;
+        int avgmarks= mathmarks+physicsmarks+chemistrymarks/3;
+        System.out.println("Sam average marks in PCM is"+avgmarks);
+    }
+}
