40dbb7b40dc8b2491891643bc9379b8df41a0227
gauravman96 <gauravmangal9660@gmail.com>
2025-12-19 16:10:05 +0530

    [gaurav mangal] Add . NaturalNumberSumByFor program added

---
+++ core-java-practice/gcr-codebase/control-flow/NaturalNumberSumByFor.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class NaturalNumberSumByFor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        if (n > 0) {
+            int formulaSum = n * (n + 1) / 2;
+            int loopSum = 0;
+            for (int i = 1; i <= n; i++) {
+                loopSum += i;
+            }
+            System.out.println("Sum using formula: " + formulaSum);
+            System.out.println("Sum using for loop " + loopSum);
+
+            if (formulaSum == loopSum) {
+                System.out.println("Both computations are correct.");
+            } else {
+                System.out.println("Both computations are not equal.");
+            }
+
+        } else {
+            System.out.println("The number is not a natural number");
+        }
+    }
+}
