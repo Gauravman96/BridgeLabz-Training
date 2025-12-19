27b1287e5a452844eb1d9dd2a72394446e4429f3
gauravman96 <gauravmangal9660@gmail.com>
2025-12-19 15:59:20 +0530

    [gaurav mangal] Add . SumUntilZero program added

---
+++ core-java-practice/gcr-codebase/control-flow/SumUntilZero.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class SumUntilZero {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double total = 0.0;
+        System.out.print("Enter a number (0 to stop) ");
+        double  number = sc.nextDouble();
+
+        while (number != 0) {
+            total += number;
+            System.out.print("Enter a number (0 to stop)");
+            number = sc.nextDouble();
+        }
+
+        System.out.println("Total sum is: " + total);
+
+    }
+}
