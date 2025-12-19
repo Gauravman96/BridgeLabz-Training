716b6525c84c2b4ceaf03b6d7aa3d7504977a36f
gauravman96 <gauravmangal9660@gmail.com>
2025-12-19 16:07:16 +0530

    [gaurav mangal] Add .NaturalNumberSumCheck program added

---
+++ core-java-practice/gcr-codebase/control-flow/NaturalNumberSumCheck.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class NaturalNumberSumCheck {
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        if (n > 0) {
+            int formulaSum = n * (n + 1) / 2;
+
+            int loopSum = 0;
+            int i = 1;
+
+            while (i <= n) {
+                loopSum += i;
+                i++;
+            }
+
+            System.out.println("Sum using formula: " + formulaSum);
+            System.out.println("Sum using while loop: " + loopSum);
+
+            if (formulaSum == loopSum) {
+                System.out.println("Both computations are correct.");
+            } else {
+                System.out.println("Both computations are not equal.");
+            }
+
+        } else {
+            System.out.println("The number is not a natural number");
+        }
+    }
+}
