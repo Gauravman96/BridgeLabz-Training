0b228ade9cf4e58503ad85dde2905c0439e3a365
gauravman96 <gauravmangal9660@gmail.com>
2025-12-19 15:22:34 +0530

    [gaurav mangal] VoteEligibility program added

---
+++ core-java-practice/gcr-codebase/control-flow/VoteEligibility.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class VoteEligibility {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter age");
+        int age = sc.nextInt();
+        // Check voting eligibility
+        if (age >= 18) {
+            System.out.println("The person's age is " + age + " and can vote.");
+        } else {
+            System.out.println("The person's age is " + age + " and cannot vote.");
+        }
+    }
+}
