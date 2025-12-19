7914e7482cef0fc8b5053873e40505beb78f398f
gauravman96 <gauravmangal9660@gmail.com>
2025-12-19 16:12:56 +0530

    [gaurav mangal] Add .FactorialWhile program added

---
+++ core-java-practice/gcr-codebase/control-flow/FactorialWhile.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class FactorialWhile {
+   public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+        if (number > 0) {
+            int factorial = 1;
+            int i = 1;
+
+            while (i <= number) {
+                factorial *= i;
+                i++;
+            }
+            System.out.println("Factorial of " + number + " is " + factorial);
+
+        } else {
+            System.out.println("Please enter a positive integer");
+        }
+   } 
+}
