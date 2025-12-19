decc9914533a2a438ee513bba7dc73bf85b15ec6
gauravman96 <gauravmangal9660@gmail.com>
2025-12-19 22:47:27 +0530

    [gaurav mangal] Add . LEVEL 3 All  program added

---
+++ core-java-practice/gcr-codebase/control-flow/HarshadNumber.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class HarshadNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number, sum = 0, temp;
+        System.out.print("Enter a number: ");
+        number = sc.nextInt();
+
+        temp = number;
+
+        while (temp != 0) {
+            int digit = temp % 10;
+            sum = sum + digit;
+            temp = temp / 10;
+        }
+
+        if (number % sum == 0) {
+            System.out.println(number + " is a Harshad Number");
+        } else {
+            System.out.println(number + " is NOT a Harshad Number");
+        }
+    }
+}
