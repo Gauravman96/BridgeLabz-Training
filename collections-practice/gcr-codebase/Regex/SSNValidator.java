15497f82ec0723648640ac19679a697dcf6cf59d
gauravman96 <gauravmangal9660@gmail.com>
2026-01-29 20:08:39 +0530

    [gaurav mangal] add . completed regex

---
+++ collections-practice/gcr-codebase/Regex/SSNValidator.java
@@ -0,0 +1,12 @@
+public class SSNValidator {
+
+    public static void main(String[] args) {
+
+        String ssn = "123-45-6789";
+
+        if (ssn.matches("^\\d{3}-\\d{2}-\\d{4}$"))
+            System.out.println("Valid SSN");
+        else
+            System.out.println("Invalid SSN");
+    }
+}
